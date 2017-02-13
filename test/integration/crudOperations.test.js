e0166f1edd7d399c021befda3f7bdecfa065fc0a
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-02-12 20:37:58 -0500

    Fix integration test script.

--- test/integration/crudOperations.test.js
+++ test/integration/crudOperations.test.js
@@ -1,16 +1,6 @@
 'use strict';
 
 const test = require('ava');
-const td = require('../helpers').td;
-
-
-test.before('setup', () => {
-
-});
-
-test.after('teardown', () => {
-  td.reset();
-});
 
 // TODO: Add crudOperations test here.
 test('crudOperations should return users', t => {
